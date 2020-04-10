5561967765e93ddab5420b082aae0f0897083411
nambii-18 <anant.nambiar@gmail.com>
2020-04-10 17:31:28 +0530

    part3.6 added

--- index.js
+++ index.js
@@ -30,6 +30,49 @@ app.get('/api/persons', (req, res) => {
     res.send(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person){
+        res.json(person)
+    }
+    else{
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if(!body.name || !body.number){
+        return res.status(400).json({
+            error: 'Content missing'
+        })
+    }
+
+    const existing = persons.find(person => person.name === body.name)
+
+    if(existing){
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const person = {
+        'name': body.name,
+        'number': body.number,
+        'id': Math.floor(Math.random()*1000)
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
 app.get('/api/info', (req, res) => {
     const maxId = persons.length
     res.send(`<div>   \
