7778a2bdbb55cdd619cdaebcc680fb3ab5b78461
nambii-18 <anant.nambiar@gmail.com>
2020-04-11 20:15:32 +0530

    part3.18 added

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+      } 
+    next(error)
+}
+app.use(errorHandler)
+
 // app.get('/', (req, res) => {
 //     res.send('<h1>Hey there</h1>')
 // })
@@ -23,27 +32,26 @@ app.get('/api/persons', (req, res) => {
         })
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person
         .findById(req.params.id)
         .then(result => {
-            res.json(result.toJSON())
+            if(result){
+                res.json(result.toJSON())
+            }
+            else{
+                console.log("Not found in DB")
+                res.status(404).end()
+            }
         })
-    const id = parseInt(req.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if(person){
-        res.json(person)
-    }
-    else{
-        res.status(404).end()
-    }
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person
+        .findByIdAndRemove(req.params.id)
+        .then(result => res.status(204).end())
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -53,14 +61,6 @@ app.post('/api/persons', (req, res) => {
             error: 'Content missing'
         })
     }
-
-    // const existing = persons.find(person => person.name === body.name)
-
-    // if(existing){
-    //     return res.status(400).json({
-    //         error: 'name must be unique'
-    //     })
-    // }
     const person = new Person({
         name: body.name,
         number: body.number
@@ -72,12 +72,35 @@ app.post('/api/persons', (req, res) => {
         })
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person
+        .findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(result => {
+            if(result){
+                res.json(result.toJSON())
+            }
+            else{
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
 app.get('/api/info', (req, res) => {
-    const maxId = persons.length
-    res.send(`<div>   \
-    Phonebook has info of ${maxId} people <br>
-    ${new Date()}\
-    </div>`)
+    Person
+        .find({})
+        .then(result => {
+            res.send(`<div>   \
+            Phonebook has info of ${result.length} people <br>
+            ${new Date()}\
+            </div>`)
+        })
 })
 
 const unknownEndpoint = (req, res) => {
